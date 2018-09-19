2ce3201a166caf76b4f4df0c71ec2fda484371f4
Michael Diener <michael@webdiener.ch>
2018-09-19 15:14:49 -0700

    Finish OpenPGP Test Cases
    All test cases for the OpenPGP class are done. Creating, deleting, 
    encrypting, decrypting and updating password are all covered through 
    tests.

--- test/test_nova_pgp.py
+++ test/test_nova_pgp.py
@@ -5,30 +5,78 @@ import nova.pgp
 import nova.exceptions
 
 
-class AccessorTest(unittest.TestCase):
+class OpenPGPTest(unittest.TestCase):
+    _connector = nova.connector.Connection()
+
+    _userid_A = 'userid_f0a1324bfe7a41f0ac36a05cf982187a'
+    _password_A = 'acc21ef11dfd40d3b65598c53b0d05dc'
+    _email_A = 'test_A@example.com'
+    _userid_B = 'userid_a142bbafe8a442b18a52f21080d1480b'
+    _password_B = '798976a856e54082b5e2e43e550880bb'
+    _email_B = 'test_B@example.com'
+    _message = 'This is a test message.'
+
     def setUp(self):
-        self._connector = nova.connector.Connection()
         self._connector.delete('_keys')
 
     def test_create_key(self):
-        userid = 'userid_f0a1324bfe7a41f0ac36a05cf982187a'
-        password = 'acc21ef11dfd40d3b65598c53b0d05dc'
-        email = 'test@example.com'
-
         pgp = nova.pgp.OpenPGP()
-        pgp.create_key(userid, email, password)
+        pgp.create_key(self._userid_A, self._password_A, self._email_A)
 
-        self.assertIsNotNone(self._connector.get('_keys', userid))
+        self.assertIsNotNone(self._connector.get('_keys', self._userid_A))
 
-    def test_encrypt(self):
-        userid_A = 'userid_f0a1324bfe7a41f0ac36a05cf982187a'
-        password_A = 'acc21ef11dfd40d3b65598c53b0d05dc'
-        email_A = 'test_A@example.com'
-        userid_B = 'userid_f0a1324bfe7a41f0ac36a05cf982187a'
-        password_B = 'acc21ef11dfd40d3b65598c53b0d05dc'
-        email_B = 'test_B@example.com'
-
+    def test_encrypt_decrypt(self):
         pgp = nova.pgp.OpenPGP()
-        pgp.create_key(self._userid, self._email, self._password)
 
-        message = 'This is a test message.'
+        pgp.create_key(self._userid_A, self._email_A, self._password_A)
+        pgp.create_key(self._userid_B, self._email_B, self._password_B)
+
+        with self.assertRaises(nova.exceptions.ParameterError):
+            pgp.encrypt(True, self._userid_A)
+            pgp.encrypt(self._message, 120)
+
+        with self.assertRaises(nova.exceptions.EncryptionError):
+            pgp.encrypt(self._message, 'userid_undefined')
+
+        encrypted_message = pgp.encrypt(self._message, self._userid_A)
+        decrypted_message = pgp.decrypt(encrypted_message, self._userid_A, self._password_A)
+
+        self.assertEqual(self._message, decrypted_message)
+
+        with self.assertRaises(nova.exceptions.DecryptionError):
+            decrypted_message = pgp.decrypt(encrypted_message, self._userid_B, self._password_B)
+
+        encrypted_message = pgp.encrypt(self._message, [self._userid_A, self._userid_B])
+        decrypted_message_A = pgp.decrypt(encrypted_message, self._userid_A, self._password_A)
+        decrypted_message_B = pgp.decrypt(encrypted_message, self._userid_B, self._password_B)
+
+        self.assertEqual(self._message, decrypted_message_A)
+        self.assertEqual(self._message, decrypted_message_B)
+
+    def test_delete_key(self):
+        pgp = nova.pgp.OpenPGP()
+        pgp.create_key(self._userid_A, self._email_A, self._password_A)
+
+        with self.assertRaises(nova.exceptions.ParameterError):
+            pgp.delete_key(123908)
+
+        pgp.delete_key(self._userid_A)
+
+        self.assertIsNone(self._connector.get('_keys', self._userid_A))
+
+    def test_update_password(self):
+        pgp = nova.pgp.OpenPGP()
+        pgp.create_key(self._userid_A, self._email_A, self._password_A)
+
+        with self.assertRaises(nova.exceptions.ParameterError):
+            pgp.update_password(120931, self._password_A, self._password_B)
+            pgp.update_password(self._userid_A, 8309123, self._password_B)
+            pgp.update_password(self._userid_A, self._password_A, 1289379)
+
+        encrypted_message = pgp.encrypt(self._message, self._userid_A)
+
+        pgp.update_password(self._userid_A, self._password_A, self._password_B)
+
+        decrypted_message = pgp.decrypt(encrypted_message, self._userid_A, self._password_B)
+
+        self.assertEqual(self._message, decrypted_message)
