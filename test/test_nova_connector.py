1cd4774d641ed9cffda7e360332ed11e1a3967ca
Michael Diener <michael@webdiener.ch>
2018-09-19 15:12:49 -0700

    Remove test data from setup and make it a class variable

--- test/test_nova_connector.py
+++ test/test_nova_connector.py
@@ -6,15 +6,15 @@ import nova.exceptions
 
 
 class ConnectorTest(unittest.TestCase):
-    def setUp(self):
-        self._test_data = {
-            'string': 'foobar',
-            'number': 10.4,
-            'boolean': True,
-            'list': [1, 2, 3],
-            'dictionary': {'one': 'two'}
-        }
+    _test_data = {
+        'string': 'foobar',
+        'number': 10.4,
+        'boolean': True,
+        'list': [1, 2, 3],
+        'dictionary': {'one': 'two'}
+    }
 
+    def setUp(self):
         redis_client = redis.StrictRedis(host='127.0.0.1', port=6379, db=1)
         redis_client.execute_command('DEL __unittest')
         redis_client.execute_command('JSON.SET', '__unittest', '.', '{}')
